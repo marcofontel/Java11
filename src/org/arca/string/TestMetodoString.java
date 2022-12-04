20aab3eec7f90d9053649d8d4252a1580bbc8ba8
server <wll.rgr@gmail.com>
2022-12-04 13:29:29 -0300

    Teste metodo lines()

--- src/org/arca/string/TestMetodoString.java
+++ src/org/arca/string/TestMetodoString.java
@@ -1,11 +1,19 @@
 package org.arca.string;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class TestMetodoString {
 
 	
 	public static void main(String[] args) {
 		var str1 = " ";
 		var str2 = "";
-		var str3 = "Tem Conteudo";
+		var str3 = "Tem\nConteudo\nAvançado";
+		System.out.println(str3);
+		
+		System.out.println(str3.lines().collect(Collectors.toList()));
+		
+		
 	}
 }
